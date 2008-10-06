ea0a9dda2561256709443ff9c17347b719643a3e
Matt Jankowski <mjankowski@thoughtbot.com>
2008-10-05 23:33:55 -0400

    add _after and _before methods for datetime

--- lib/pacecar/datetime.rb
+++ lib/pacecar/datetime.rb
@@ -16,6 +16,12 @@ module Pacecar
         datetime_columns.each do |column|
           named_scope column.to_sym, :conditions => "#{column} is not null"
           named_scope "not_#{column}".to_sym, :conditions => "#{column} is null"
+          named_scope "#{column}_before".to_sym, lambda { |time|
+            { :conditions => ["#{column} < ?", time] }
+          }
+          named_scope "#{column}_after".to_sym, lambda { |time|
+            { :conditions => ["#{column} > ?", time] }
+          }
         end
       end
 
--- test/datetime_test.rb
+++ test/datetime_test.rb
@@ -6,19 +6,39 @@ class DatetimeTest < Test::Unit::TestCase
     setup do
       @class = User
     end
-    should "respond to datetime column method" do
-      assert @class.respond_to? :created_at
+    context "with presence methods" do
+      should "respond to datetime column method" do
+        assert @class.respond_to? :created_at
+      end
+      should "set correct proxy options for datetime column method" do
+        proxy_options = { :conditions => 'created_at is not null' }
+        assert_equal proxy_options, @class.created_at.proxy_options
+      end
+      should "respond to not_ datetime column method" do
+        assert @class.respond_to? :not_created_at
+      end
+      should "set correct proxy options for not_ datetime column method" do
+        proxy_options = { :conditions => 'created_at is null' }
+        assert_equal proxy_options, @class.not_created_at.proxy_options
+      end
     end
-    should "set correct proxy options for datetime column method" do
-      proxy_options = { :conditions => 'created_at is not null' }
-      assert_equal proxy_options, @class.created_at.proxy_options
-    end
-    should "respond to not_ datetime column method" do
-      assert @class.respond_to? :not_created_at
-    end
-    should "set correct proxy options for not_ datetime column method" do
-      proxy_options = { :conditions => 'created_at is null' }
-      assert_equal proxy_options, @class.not_created_at.proxy_options
+    context "with before and after methods" do
+      should "respond to _before datetime column method" do
+        assert @class.respond_to? :created_at_before
+      end
+      should "set correct proxy options for _before datetime column method" do
+        time = 5.days.ago
+        proxy_options = { :conditions => ['created_at < ?', time] }
+        assert_equal proxy_options, @class.created_at_before(time).proxy_options
+      end
+      should "respond to _after datetime column method" do
+        assert @class.respond_to? :created_at_after
+      end
+      should "set correct proxy options for after_ datetime column method" do
+        time = 5.days.ago
+        proxy_options = { :conditions => ['created_at > ?', time] }
+        assert_equal proxy_options, @class.created_at_after(time).proxy_options
+      end
     end
   end
 

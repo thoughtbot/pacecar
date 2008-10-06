b1b12caad5e002800a81cc305f323e30e6829be9
Matt Jankowski <mjankowski@thoughtbot.com>
2008-10-05 23:05:10 -0400

    consistent naming

--- test/datetime_test.rb
+++ test/datetime_test.rb
@@ -13,10 +13,10 @@ class DatetimeTest < Test::Unit::TestCase
       proxy_options = { :conditions => 'created_at is not null' }
       assert_equal proxy_options, @class.created_at.proxy_options
     end
-    should "respond to datetime not_ column method" do
+    should "respond to not_ datetime column method" do
       assert @class.respond_to? :not_created_at
     end
-    should "set correct proxy options for datetime not_ column method" do
+    should "set correct proxy options for not_ datetime column method" do
       proxy_options = { :conditions => 'created_at is null' }
       assert_equal proxy_options, @class.not_created_at.proxy_options
     end

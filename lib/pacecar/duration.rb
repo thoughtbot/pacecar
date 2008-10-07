f82d6f6e5485689f7507ff1cebf642c28b1fc7be
Matt Jankowski <mjankowski@thoughtbot.com>
2008-10-06 20:51:39 -0400

    use one query for duration

--- lib/pacecar/duration.rb
+++ lib/pacecar/duration.rb
@@ -12,17 +12,14 @@ module Pacecar
       protected
 
       def define_duration_scopes
-        named_scope :duration_between, lambda { |start, stop|
-          { :select => "datediff(#{quoted_table_name}.#{start}, #{quoted_table_name}.#{stop}) as duration" }
+        named_scope :with_duration_of, lambda { |duration, start, stop|
+          { :conditions => ["datediff(#{quoted_table_name}.#{start}, #{quoted_table_name}.#{stop}) = ?", duration] }
         }
-        named_scope :with_duration_of, lambda { |duration|
-          { :conditions => ['duration = ?', duration] }
+        named_scope :with_duration_over, lambda { |duration, start, stop|
+          { :conditions => ["datediff(#{quoted_table_name}.#{start}, #{quoted_table_name}.#{stop}) > ?", duration] }
         }
-        named_scope :with_duration_over, lambda { |duration|
-          { :conditions => ['duration >= ?', duration] }
-        }
-        named_scope :with_duration_under, lambda { |duration|
-          { :conditions => ['duration <= ?', duration] }
+        named_scope :with_duration_under, lambda { |duration, start, stop|
+          { :conditions => ["datediff(#{quoted_table_name}.#{start}, #{quoted_table_name}.#{stop}) < ?", duration] }
         }
       end
 
--- test/duration_test.rb
+++ test/duration_test.rb
@@ -10,24 +10,26 @@ class DurationTest < Test::Unit::TestCase
       setup do
         @days = 14
       end
-      should "respond to a duration_between datetime column method" do
-        assert @class.respond_to? :duration_between
-      end
-      should "set the correct proxy options for a duration_between datetime column method" do
-        proxy_options = { :select => 'datediff("users".created_at, "users".updated_at) as duration' }
-        assert_equal proxy_options, @class.duration_between(:created_at, :updated_at).proxy_options
+      should "respond to a with_duration_of datetime column method" do
+        assert @class.respond_to? :with_duration_of
       end
       should "set the correct proxy options for a with_duration_of datetime column method" do
-        proxy_options = { :conditions => ['duration = ?', @days] }
-        assert_equal proxy_options, @class.with_duration_of(@days).proxy_options
+        proxy_options = { :conditions => ['datediff("users".created_at, "users".updated_at) = ?', @days] }
+        assert_equal proxy_options, @class.with_duration_of(@days, :created_at, :updated_at).proxy_options
+      end
+      should "respond to a with_duration_over datetime column method" do
+        assert @class.respond_to? :with_duration_of
       end
       should "set the correct proxy options for a with_duration_over datetime column method" do
-        proxy_options = { :conditions => ['duration >= ?', @days] }
-        assert_equal proxy_options, @class.with_duration_over(@days).proxy_options
+        proxy_options = { :conditions => ['datediff("users".created_at, "users".updated_at) > ?', @days] }
+        assert_equal proxy_options, @class.with_duration_over(@days, :created_at, :updated_at).proxy_options
+      end
+      should "respond to a with_duration_under datetime column method" do
+        assert @class.respond_to? :with_duration_under
       end
       should "set the correct proxy options for a with_duration_under datetime column method" do
-        proxy_options = { :conditions => ['duration <= ?', @days] }
-        assert_equal proxy_options, @class.with_duration_under(@days).proxy_options
+        proxy_options = { :conditions => ['datediff("users".created_at, "users".updated_at) < ?', @days] }
+        assert_equal proxy_options, @class.with_duration_under(@days, :created_at, :updated_at).proxy_options
       end
     end
   end

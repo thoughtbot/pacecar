3b0ade722136c79cf6f98995d09a1d591940cab8
Matt Jankowski <mjankowski@thoughtbot.com>
2008-10-06 20:12:37 -0400

    use table name in boolean

--- lib/pacecar/boolean.rb
+++ lib/pacecar/boolean.rb
@@ -13,8 +13,8 @@ module Pacecar
 
       def define_boolean_scopes
         boolean_column_names.each do |name|
-          named_scope name.to_sym, :conditions => ["#{name} = ?", true]
-          named_scope "not_#{name}".to_sym, :conditions => ["#{name} = ?", false]
+          named_scope name.to_sym, :conditions => ["#{quoted_table_name}.#{name} = ?", true]
+          named_scope "not_#{name}".to_sym, :conditions => ["#{quoted_table_name}.#{name} = ?", false]
         end
       end
 
--- test/boolean_test.rb
+++ test/boolean_test.rb
@@ -10,14 +10,14 @@ class BooleanTest < Test::Unit::TestCase
       assert @class.respond_to? :admin
     end
     should "set the correct proxy options for a boolean column method" do
-      proxy_options = { :conditions => ['admin = ?', true] }
+      proxy_options = { :conditions => ['"users".admin = ?', true] }
       assert_equal proxy_options, @class.admin.proxy_options
     end
     should "respond to a not_ boolean column method" do
       assert @class.respond_to? :not_admin
     end
     should "set the correct proxy options for a not_ boolean column method" do
-      proxy_options = { :conditions => ['admin = ?', false] }
+      proxy_options = { :conditions => ['"users".admin = ?', false] }
       assert_equal proxy_options, @class.not_admin.proxy_options
     end
   end

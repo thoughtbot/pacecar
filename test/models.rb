fa1d7ebd55fa4d432787c2e3ac0d8b818fbde90e
Matt Jankowski <mjankowski@thoughtbot.com>
2009-04-08 19:51:23 -0400

    freeze constants

--- test/models.rb
+++ test/models.rb
@@ -36,8 +36,8 @@ class User < ActiveRecord::Base
 end
 class Post < ActiveRecord::Base
   include Pacecar
-  PUBLICATION_STATES = %w(Draft Submitted Rejected Accepted)
-  TYPES = %w(Free Open Private Anonymous PostModern)
+  PUBLICATION_STATES = %w(Draft Submitted Rejected Accepted).freeze
+  TYPES = %w(Free Open Private Anonymous PostModern).freeze
   belongs_to :owner, :polymorphic => true
   has_state :publication_state
   has_state :post_type, :with => TYPES

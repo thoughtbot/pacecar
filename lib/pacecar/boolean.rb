e628a1dc9b4074b067904ea58aad8245a8f0b788
Matt Jankowski <mjankowski@thoughtbot.com>
2009-04-08 20:03:50 -0400

    add balance methods to boolean module

--- lib/pacecar/boolean.rb
+++ lib/pacecar/boolean.rb
@@ -7,6 +7,7 @@ module Pacecar
     module ClassMethods
       def self.extended(base)
         base.send :define_boolean_scopes
+        base.send :define_balance_count
       end
 
       protected
@@ -15,6 +16,16 @@ module Pacecar
         boolean_column_names.each do |name|
           named_scope name.to_sym, :conditions => ["#{quoted_table_name}.#{name} = ?", true]
           named_scope "not_#{name}".to_sym, :conditions => ["#{quoted_table_name}.#{name} = ?", false]
+        end
+      end
+
+      def define_balance_count
+        boolean_column_names.each do |name|
+          self.class_eval %Q{
+            def self.#{name}_balance
+              #{name}.count - not_#{name}.count
+            end
+          }
         end
       end
 
--- test/boolean_test.rb
+++ test/boolean_test.rb
@@ -16,6 +16,25 @@ class BooleanTest < Test::Unit::TestCase
       proxy_options = { :conditions => ['"users".admin = ?', false] }
       assert_equal proxy_options, @class.not_admin.proxy_options
     end
+    context "With boolean column scopes that can count" do
+      setup do
+        assert @class.respond_to?(:admin_balance)
+        @true_mock = mock
+        @false_mock = mock
+        @class.expects(:admin).returns @true_mock
+        @class.expects(:not_admin).returns @false_mock
+      end
+      should "return correct value for balance class method when true greater than false" do
+        @true_mock.expects(:count).returns 5
+        @false_mock.expects(:count).returns 2
+        assert_equal 3, @class.admin_balance
+      end
+      should "return correct value for balance class method when true less than false" do
+        @true_mock.expects(:count).returns 2
+        @false_mock.expects(:count).returns 5
+        assert_equal -3, @class.admin_balance
+      end
+    end
   end
 
 end

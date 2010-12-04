a4193ac8ef13262275cd33b1af58a2c4c6b444ed
Matt Jankowski <mjankowski@thoughtbot.com>
2010-12-04 15:06:08 -0500

    make sure we migrate every time

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,6 +15,9 @@ require "capybara/rails"
 Capybara.default_driver   = :rack_test
 Capybara.default_selector = :css
 
+# Delete DB from previous runs
+FileUtils.rm File.expand_path("../dummy/db/test.sqlite3", __FILE__)
+
 # Run any available migration
 ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
 

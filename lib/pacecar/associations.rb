1e487538d4503a0a693adfd2eb784a7667cbe414
Ryan Sonnek <ryan.sonnek@gmail.com>
2011-09-20 14:48:09 -0500

    fix deprecation warnings

--- lib/pacecar/associations.rb
+++ lib/pacecar/associations.rb
@@ -31,7 +31,7 @@ module Pacecar
       protected
 
       def conditions_for_name(name)
-        "((select count(*) from #{connection.quote_table_name(name)} where #{connection.quote_table_name(name)}.#{connection.quote_column_name (reflections[name].respond_to?(:foreign_key) ? reflections[name].foreign_key : reflections[name].primary_key_name)} = #{quoted_table_name}.#{connection.quote_column_name primary_key} and #{connection.quote_table_name(name)}.#{connection.quote_column_name("created_at")} > :since_time) > 0)"
+        "((select count(*) from #{connection.quote_table_name(name)} where #{connection.quote_table_name(name)}.#{connection.quote_column_name(reflections[name].respond_to?(:foreign_key) ? reflections[name].foreign_key : reflections[name].primary_key_name)} = #{quoted_table_name}.#{connection.quote_column_name primary_key} and #{connection.quote_table_name(name)}.#{connection.quote_column_name("created_at")} > :since_time) > 0)"
       end
 
     end

137f371320b65339484c77cf3d18dd40b5742bee
Matt Jankowski <mjankowski@thoughtbot.com>
2015-02-20 20:54:16 +0000

    Clean up state scope creation

--- lib/pacecar/state.rb
+++ lib/pacecar/state.rb
@@ -11,28 +11,40 @@ module Pacecar
         names.each do |name|
           constant = opts[:with] || const_get(name.to_s.pluralize.upcase)
           constant.each do |state|
-            scope "#{name}_#{state.downcase}", -> {
-              where(arel_table[name].eq state)
-            }
-            scope "#{name}_not_#{state.downcase}", -> {
-              where(arel_table[name].not_eq state)
-            }
-            self.class_eval %Q{
-              def #{name}_#{state.downcase}?
-                #{name} == '#{state}'
-              end
-              def #{name}_not_#{state.downcase}?
-                #{name} != '#{state}'
-              end
-            }
+            define_state_scopes(name, state)
+            define_state_queries(name, state)
           end
-          scope "#{name}", ->(state) {
-            where(arel_table[name].eq state)
-          }
-          scope "#{name}_not", ->(state) {
-            where(arel_table[name].not_eq state)
-          }
+          define_where_scopes(name)
         end
+      end
+
+      def define_state_scopes(name, state)
+        scope "#{name}_#{state.downcase}", -> {
+          where(arel_table[name].eq state)
+        }
+        scope "#{name}_not_#{state.downcase}", -> {
+          where(arel_table[name].not_eq state)
+        }
+      end
+
+      def define_state_queries(name, state)
+        self.class_eval %Q{
+          def #{name}_#{state.downcase}?
+            #{name} == '#{state}'
+          end
+          def #{name}_not_#{state.downcase}?
+            #{name} != '#{state}'
+          end
+        }
+      end
+
+      def define_where_scopes(name)
+        scope "#{name}", ->(state) {
+          where(arel_table[name].eq state)
+        }
+        scope "#{name}_not", ->(state) {
+          where(arel_table[name].not_eq state)
+        }
       end
 
     end
